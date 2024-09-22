a3bde523981b305d45634703961768a7bd3c69d8
M. Afief Anugrah <mafiefanugrah01@gmail.com>
2024-09-22 20:40:57 +0700

    routes user ready

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const port = 3300
 
+const userEnpoind = require('./Routes/users')
+
 const app = express()
 
+app.use('/', userEnpoind)
 
 app.listen(port, () => { console.log(`server runing to port ${port}`)})
