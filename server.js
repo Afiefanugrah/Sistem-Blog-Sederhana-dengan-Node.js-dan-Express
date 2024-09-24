790793877bf6d9eb0f3673c81683d2bd76d84d24
M. Afief Anugrah <mafiefanugrah01@gmail.com>
2024-09-24 19:18:23 +0700

    menambhakan cors

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
+const cors = require('cors')
 const port = 3300
 
-const sequelize = require('./db.config')
+
 
 const userEnpoind = require('./Routes/usersRoutes')
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
 app.use('/', userEnpoind)
