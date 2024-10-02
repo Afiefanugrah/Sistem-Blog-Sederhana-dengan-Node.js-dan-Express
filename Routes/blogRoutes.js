f0cfa2d78660a4cd2f0ceba334b7db1c185705f9
M. Afief Anugrah <mafiefanugrah01@gmail.com>
2024-10-02 23:31:25 +0700

    delete

--- Routes/blogRoutes.js
+++ Routes/blogRoutes.js
@@ -44,20 +44,7 @@ router.get('/posts/:id', async (req, res) => {
   }
 })
 
-router.put('/posts/:id', async (req, res) => {
-  const {id} = req.params
-  const blog = await blogModel.findByPk(id)
-  if(blog) {
-    res.json({
-      data: blog,
-      metadata: "blog by id endpoint"
-    })
-  } else{
-    res.json({error: "data invalid"})
-  }
-})
-
-router.put('/posts/:id', async (req, res) => {
+router.delete('/posts/:id', async (req, res) => {
   const {id} = req.params
   const blog = await blogModel.findByPk(id)
   if(blog) {
